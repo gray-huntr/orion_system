c51eeb78700c68d36b6406a130cefe6a41c66cd5
gray-huntr <lkulecho2001@gmail.com>
2023-11-16 20:24:51 +0300

    404 page handler added

--- myapp/admin.py
+++ myapp/admin.py
@@ -198,5 +198,7 @@ def search(category):
             flash("There is no record with the specified search term, try again", "info")
             return redirect("/billing")
 
-
-
+# Custom error handler for 404 Not Found
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
